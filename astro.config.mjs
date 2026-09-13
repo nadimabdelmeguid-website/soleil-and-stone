0c9c8e78554bc8f497bd3c80522fc04a1d936667
nadimabdelmeguid-website <nadimabdelmeguid@gmail.com>
2026-09-13 00:26:59 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://soleilandstone.co"
+  site: "https://soleilandstone.co",
+
+  integrations: [
+    sitemap()
+  ]
 });
