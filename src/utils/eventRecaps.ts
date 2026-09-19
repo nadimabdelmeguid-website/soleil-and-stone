bdfa9d43af4f8b65a2cc3e05ccdfc6ea4be89950
nadimabdelmeguid-website <nadimabdelmeguid@gmail.com>
2026-09-19 14:17:43 -0700

    Use CMS recap links

--- src/utils/eventRecaps.ts
+++ src/utils/eventRecaps.ts
@@ -1,33 +1,17 @@
 import {
-  publishedEventRecaps
-} from "../data/eventRecaps";
-
-
-export function getEventRecapByEventId(
-  eventId: string
-) {
-
-  return publishedEventRecaps.find(
-    (recap) =>
-      recap.eventId ===
-        eventId
-  );
-
-}
+  eventRecapLinks
+} from "../data/eventRecapLinks.cms";
 
 
 export function getEventRecapUrl(
   eventId: string
 ) {
 
-  const recap =
-    getEventRecapByEventId(
-      eventId
-    );
-
-
-  return recap
-    ? `/event-recaps/${recap.slug}/`
-    : null;
+  return (
+    eventRecapLinks[
+      eventId as keyof typeof eventRecapLinks
+    ] ??
+    null
+  );
 
 }
